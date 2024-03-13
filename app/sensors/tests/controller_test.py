57be54598278567b7419879dc00a8524533c5a50
Miriam Martinez <miriam.martinez.mc@gmail.com>
2024-03-13 02:57:01 +0100

    Revert "change order of near test, put before remove the data"
    This reverts commit 53feeec8fdbe2ce4069a2b252b44584fb7f82c21.

--- app/sensors/tests/controller_test.py
+++ app/sensors/tests/controller_test.py
@@ -1,74 +1,59 @@
-import pytest
 from fastapi.testclient import TestClient
-
+import pytest
 from app.main import app
-from app.mongodb_client import MongoDBClient
 from app.redis_client import RedisClient
+from app.mongodb_client import MongoDBClient
 
 client = TestClient(app)
 
 
+
 @pytest.fixture(scope="session", autouse=True)
 def clear_dbs():
-    from app.database import engine
-    from app.sensors import models
-    models.Base.metadata.drop_all(bind=engine)
-    models.Base.metadata.create_all(bind=engine)
-    redis = RedisClient(host="redis")
-    redis.clearAll()
-    redis.close()
-    mongo = MongoDBClient(host="mongodb")
-    mongo.clearDb("sensors")
-    mongo.close()
-
+     from app.database import SessionLocal, engine
+     from app.sensors import models
+     models.Base.metadata.drop_all(bind=engine)
+     models.Base.metadata.create_all(bind=engine)
+     redis = RedisClient(host="redis")
+     redis.clearAll()
+     redis.close()
+     mongo = MongoDBClient(host="mongodb")
+     mongo.clearDb("sensors")
+     mongo.close()
 
 def test_create_sensor_temperatura():
-    """A sensor can be properly created"""
-    response = client.post("/sensors", json={"name": "Sensor Temperatura 1", "latitude": 1.0, "longitude": 1.0,
-                                             "type": "Temperatura", "mac_address": "00:00:00:00:00:00",
-                                             "manufacturer": "Dummy", "model": "Dummy Temp",
-                                             "serie_number": "0000 0000 0000 0000", "firmware_version": "1.0"})
-    assert response.status_code == 200
-    json = response.json()
-    assert json["id"] == 1
-    assert json["name"] == "Sensor Temperatura 1"
-
+     """A sensor can be properly created"""
+     response = client.post("/sensors", json={"name": "Sensor Temperatura 1", "latitude": 1.0, "longitude": 1.0, "type": "Temperatura", "mac_address": "00:00:00:00:00:00", "manufacturer": "Dummy", "model":"Dummy Temp", "serie_number": "0000 0000 0000 0000", "firmware_version": "1.0"})
+     assert response.status_code == 200
+     json = response.json()
+     assert json["id"] == 1
+     assert json["name"] == "Sensor Temperatura 1"
 
 def test_create_sensor_velocitat():
-    response = client.post("/sensors",
-                           json={"name": "Sensor Velocitat 1", "latitude": 1.0, "longitude": 1.0, "type": "Velocitat",
-                                 "mac_address": "00:00:00:00:00:01", "manufacturer": "Dummy", "model": "Dummy Vel",
-                                 "serie_number": "0000 0000 0000 0000", "firmware_version": "1.0"})
+    response = client.post("/sensors", json={"name": "Sensor Velocitat 1", "latitude": 1.0, "longitude": 1.0, "type": "Velocitat", "mac_address": "00:00:00:00:00:01", "manufacturer": "Dummy", "model":"Dummy Vel", "serie_number": "0000 0000 0000 0000", "firmware_version": "1.0"})
     assert response.status_code == 200
     assert response.status_code == 200
     json = response.json()
     assert json["id"] == 2
     assert json["name"] == "Sensor Velocitat 1"
 
-
 def test_redis_connection():
     redis_client = RedisClient(host="redis")
     assert redis_client.ping()
     redis_client.close()
-
 
 def test_mongodb_connection():
     mongodb_client = MongoDBClient(host="mongodb")
     assert mongodb_client.ping()
     mongodb_client.close()
 
-
 def test_post_sensor_1_data_():
-    response = client.post("/sensors/1/data", json={"temperature": 1.0, "humidity": 1.0, "battery_level": 1.0,
-                                                    "last_seen": "2020-01-01T00:00:00.000Z"})
+    response = client.post("/sensors/1/data", json={"temperature": 1.0, "humidity": 1.0, "battery_level": 1.0, "last_seen": "2020-01-01T00:00:00.000Z"})
     assert response.status_code == 200
-
 
 def test_post_sensor_2_data():
-    response = client.post("/sensors/2/data",
-                           json={"velocity": 45.0, "battery_level": 1.0, "last_seen": "2020-01-01T00:00:00.000Z"})
+    response = client.post("/sensors/2/data", json={"velocity": 45.0,"battery_level": 1.0, "last_seen": "2020-01-01T00:00:00.000Z"})
     assert response.status_code == 200
-
 
 def test_get_sensor_1_data():
     """We can get a sensor by its id"""
@@ -82,7 +67,6 @@ def test_get_sensor_1_data():
     assert json["battery_level"] == 1.0
     assert json["last_seen"] == "2020-01-01T00:00:00.000Z"
 
-
 def test_get_sensor_2_data():
     """We can get a sensor by its id"""
     response = client.get("/sensors/2/data")
@@ -94,31 +78,23 @@ def test_get_sensor_2_data():
     assert json["battery_level"] == 1.0
     assert json["last_seen"] == "2020-01-01T00:00:00.000Z"
 
-
 def test_post_sensor_data_not_exists():
-    response = client.post("/sensors/3/data", json={"temperature": 1.0, "humidity": 1.0, "battery_level": 1.0,
-                                                    "last_seen": "2020-01-01T00:00:00.000Z"})
+    response = client.post("/sensors/3/data", json={"temperature": 1.0, "humidity": 1.0, "battery_level": 1.0, "last_seen": "2020-01-01T00:00:00.000Z"})
     assert response.status_code == 404
     assert "Sensor not found" in response.text
-
 
 def test_get_sensor_data_not_exists():
     response = client.get("/sensors/3/data")
     assert response.status_code == 404
     assert "Sensor not found" in response.text
 
-
 def test_update_sensor_1_data():
-    response = client.post("/sensors/1/data", json={"temperature": 2.0, "humidity": 2.0, "battery_level": 1.9,
-                                                    "last_seen": "2020-01-01T00:00:01.000Z"})
+    response = client.post("/sensors/1/data", json={"temperature": 2.0, "humidity": 2.0, "battery_level": 1.9, "last_seen": "2020-01-01T00:00:01.000Z"})
     assert response.status_code == 200
-
 
 def test_update_sensor_2_data():
-    response = client.post("/sensors/2/data",
-                           json={"velocity": 46.0, "battery_level": 1.9, "last_seen": "2020-01-01T00:00:01.000Z"})
+    response = client.post("/sensors/2/data", json={"velocity": 46.0,"battery_level": 1.9, "last_seen": "2020-01-01T00:00:01.000Z"})
     assert response.status_code == 200
-
 
 def test_get_sensor_1_data_updated():
     """We can get a sensor by its id"""
@@ -144,6 +120,13 @@ def test_get_sensor_2_data_updated():
     assert json["battery_level"] == 1.9
     assert json["last_seen"] == "2020-01-01T00:00:01.000Z"
 
+def test_delete_sensor_1():
+    response = client.delete("/sensors/1")
+    assert response.status_code == 200
+
+def test_delete_sensor_2():
+    response = client.delete("/sensors/2")
+    assert response.status_code == 200
 
 def test_get_near():
     response = client.get("/sensors/near?latitude=1.0&longitude=1.0&radius=1")
@@ -160,13 +143,3 @@ def test_get_near():
     assert json[1]["velocity"] == 46.0
     assert json[1]["battery_level"] == 1.9
     assert json[1]["last_seen"] == "2020-01-01T00:00:01.000Z"
-
-
-def test_delete_sensor_1():
-    response = client.delete("/sensors/1")
-    assert response.status_code == 200
-
-
-def test_delete_sensor_2():
-    response = client.delete("/sensors/2")
-    assert response.status_code == 200
